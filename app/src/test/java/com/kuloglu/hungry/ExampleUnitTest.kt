c53312ed6bb11897795bcb5f413c954ebc09db88
kuloglu <mustafakuloglunun@gmail.com>
2019-06-30 21:32:23 +0300

    test class cleaned

--- app/src/test/java/com/kuloglu/hungry/ExampleUnitTest.kt
+++ app/src/test/java/com/kuloglu/hungry/ExampleUnitTest.kt
@@ -1,16 +1,8 @@
 package com.kuloglu.hungry
 
-import com.kuloglu.hungry.utils.Validator
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
-import org.junit.Before
 import org.junit.runner.RunWith
-import org.mockito.BDDMockito.given
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 /**
@@ -22,25 +14,9 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
 
-lateinit var validator: Validator
-    @Before
-    fun setup(){
-        validator = Validator()
-    }
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
 
-    @Test
-    fun ss (){
-        assertFalse(validator.isEMailValid("mustaf@agmail.com"))
-    }
-
-    @Test
-    fun tttt(){
-        val mo = mock(Validator::class.java)
-
-        mo?.isEMailValid("mustafagmail.com")?.let { assertFalse(it) }
-    }
 }
